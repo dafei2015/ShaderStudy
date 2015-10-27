df4b8cde8baf97956a0c5d26883614f572c711fb
dafei2015 <649220234@qq.com>
2015-10-27 18:04:08 +0800

    更新了Tookit

---
+++ Assets/QianMo's Toolkit v1.1/ShowUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//添加组件菜单
+[AddComponentMenu("浅墨's Toolkit v1.1/ShowUI")]
+public class ShowUI : MonoBehaviour
+{
+    public Texture2D midBottomPic;//用于修饰的横条
+
+    void OnGUI()
+    {
+        if (midBottomPic)
+        {
+            //--------------------------【中下方横条的绘制】-------------------------
+            GUI.DrawTexture(new Rect(Screen.width / 2 - midBottomPic.width / 2, 0, midBottomPic.width, midBottomPic.height), midBottomPic);
+        }
+
+    }
+}
